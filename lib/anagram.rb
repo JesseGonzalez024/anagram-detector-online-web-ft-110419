f34e8acb2ae6ce0f041fbf4c37f368abf6581b1a
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-18 20:49:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  
+  attr_accessor :words
+  
+  def initialize(word)
+    words = word
+  end            
+  
+  def match (other_words)
+    other_words.each |word|
+      word
+    
+  end
+    binding.pry
+  
+  
+  
+end
